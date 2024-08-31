d400e0a88292897cd5621bed3d4bffee49e7614b
wanderfulife <john.wander@hotmail.fr>
2024-08-31 16:32:37 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					// Example: Split 'node_modules' into a separate chunk
+					vendor: ["vue", "vue-router", "vuex"],
+				},
+			},
+		},
+		chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+	},
+});
