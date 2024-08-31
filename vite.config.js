1520696d4d04faaeb204250287381595657d2472
wanderfulife <john.wander@hotmail.fr>
2024-08-31 16:47:25 +0200

    json

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import vue from "@vitejs/plugin-vue";
 import json from "@rollup/plugin-json";
 
 export default defineConfig({
-	plugins: [
-		vue(),
-		json({
-			compact: true,
-		}),
-	],
+	plugins: [vue(), json()],
 });
